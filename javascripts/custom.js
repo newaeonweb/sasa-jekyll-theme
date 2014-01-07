76d6c5b6d50db689a364da22c1614bf3cf2ac948
f <f>
2014-01-07 11:26:42 -0200

    add responsive menu

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -4,3 +4,18 @@ jQuery(function() {
     return $(ele).html(prettyDate(new Date($(this).data("time"))));
   });
 });
+
+;(function ($) {
+  // Simple and efective Responsive Menu
+  $("#nav").before('<div id="menu"><b>Menu</b> <span>&#9776</span> </div>');
+    $("#menu").on('click', function () {
+        $("#nav").slideToggle().on('click', function(){
+            $(this).slideUp();
+        });
+    });
+    $(window).resize(function () {
+        if (window.innerWidth > 768) {
+            $("#nav").removeAttr("style");
+        }
+    });
+})(jQuery);
